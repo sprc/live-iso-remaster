a384239c269fd07060d63e35016f03c94babcafe
Clinton Sprain <clinton.sprain@gmail.com>
2015-07-15 22:44:44 -0500

    Tweaks

--- iso-chroot.sh
+++ iso-chroot.sh
@@ -52,7 +52,7 @@ cd $path/tmp
 
 sudo mount --bind /dev/ edit/dev
 sudo mount -o bind /run/ edit/run
-sudo mount -o bind /proc/ edit/proc
+sudo mount --bind /proc/ edit/proc
 
 echo ""
 echo "   Entering chroot..."
@@ -73,7 +73,6 @@ echo ""
 bash
 
 cd $path
-
 #sudo rm linux-samus-ubuntu-0.2.2.tar
 sudo rm $path/tmp/edit/iso-chroot-main.sh
 cd tmp
@@ -95,7 +94,8 @@ sudo umount $path/tmp/edit/run
 sudo umount $path/tmp/edit/proc
 
 sudo rm extract-cd/casper/filesystem.squashfs
-sudo mksquashfs edit extract-cd/casper/filesystem.squashfs -comp lzo
+#sudo mksquashfs edit extract-cd/casper/filesystem.squashfs -comp lzo
+sudo mksquashfs edit extract-cd/casper/filesystem.squashfs
 
 sudo bash -c "printf $(sudo du -sx --block-size=1 edit | cut -f1) > extract-cd/casper/filesystem.size"
 
@@ -108,7 +108,7 @@ find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo t
 
 #sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $path/tmp/custom.iso .
 #sudo xorriso -as mkisofs -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 4 -b isolinux/isolinux.bin -c isolinux/boot.cat -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o $path/tmp/custom.iso .
-sudo xorriso -as mkisofs -o $path/tmp/custom.iso -isohybrid-mbr /usr/lib/syslinux/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table CD_root
+sudo xorriso -as mkisofs -o $path/tmp/custom.iso -isohybrid-mbr /usr/lib/syslinux/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table .
 #sudo isohybrid $path/tmp/custom.iso
 #sudo isohybrid --uefi $path/tmp/custom.iso
 
